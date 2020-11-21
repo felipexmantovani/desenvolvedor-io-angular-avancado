d355dc8bcd6c7dd98194218ab1a91d21cbc809e5
Felipe Mantovani <felipexmantovani@gmail.com>
2020-11-21 09:29:38 -0300

    Teste AppComponent

---
+++ front-end/src/app/app.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from "./app.component";
+import { LayoutComponentsModule } from './modules/layout/components/layout-components.module';
+
+describe('app.component.spec | AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [
+        BrowserModule,
+        LayoutComponentsModule,
+        RouterTestingModule
+      ]
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('Deve ser criado', () => {
+    expect(component).toBeTruthy();
+  });
+});
