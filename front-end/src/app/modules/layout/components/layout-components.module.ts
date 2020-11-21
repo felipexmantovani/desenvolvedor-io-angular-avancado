0a84fca465141f50cde4d8c85cd3cce8e290c4ad
Felipe Mantovani <felipexmantovani@gmail.com>
2020-11-21 01:33:00 -0300

    Update layout-components.module.ts

--- front-end/src/app/modules/layout/components/layout-components.module.ts
+++ front-end/src/app/modules/layout/components/layout-components.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { PoModule } from '@po-ui/ng-components';
+import { PoMenuModule } from '@po-ui/ng-components';
 import { LayoutBaseComponent } from './layout-base/layout-base.component';
 
 @NgModule({
   declarations: [LayoutBaseComponent],
-  imports: [CommonModule, PoModule, RouterModule],
+  imports: [CommonModule, PoMenuModule, RouterModule],
   exports: [LayoutBaseComponent],
 })
 export class LayoutComponentsModule {}
