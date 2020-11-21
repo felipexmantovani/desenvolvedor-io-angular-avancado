5d7a6087a893510b05f9940d46adf7a02c3c1d4e
Felipe Mantovani <felipexmantovani@gmail.com>
2020-11-21 11:53:11 -0300

    Update string.util.spec.ts

--- front-end/src/app/shared/utils/string.util.spec.ts
+++ front-end/src/app/shared/utils/string.util.spec.ts
@@ -10,22 +10,25 @@ describe('string.util.spec | StringUtil', () => {
   describe('resume()', () => {
     it('Deve resumir para 3 caracteres', () => {
       textResume = StringUtil.resume(textResume, 3);
-      expect(textResume).toEqual('Pro');
+      expect(textResume).toBe('Pro');
     });
 
     it('Deve resumir para 7 caracteres e adicionar reticencias no final', () => {
       textResume = StringUtil.resume(textResume, 7, true);
-      expect(textResume).toEqual('Program...');
+      expect(textResume).toBe('Program...');
     });
 
     it('Deve resumir para 7 caracteres e não adicionar reticencias no final', () => {
       textResume = StringUtil.resume(textResume, 7);
-      expect(textResume).toEqual('Program');
+      expect(textResume).toBe('Program');
     });
 
     it('Deve resumir somente se quantidade de caracteres do parâmetro "value" for maior ou igual ao parâmetro "maxCharacters"', () => {
-      textResume = StringUtil.resume(textResume, 15, true);
-      expect(textResume).toEqual('Programador');
+      textResume = StringUtil.resume(textResume, 15);
+      expect(textResume).toBe('Programador');
+
+      textResume = StringUtil.resume(textResume, 3);
+      expect(textResume).toBe('Pro');
     });
   });
 });
