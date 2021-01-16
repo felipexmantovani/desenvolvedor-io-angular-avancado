226af94421dccd84b0d22151f52b29ee2f2eb138
Felipe Mantovani <felipexmantovani@gmail.com>
2021-01-16 19:28:56 -0300

    Update fornecedor-read.resolver.ts

--- front-end/src/app/modules/fornecedor/resolvers/fornecedor-read.resolver.ts
+++ front-end/src/app/modules/fornecedor/resolvers/fornecedor-read.resolver.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { LoadingService } from '../../../core/modules/loading/loading.service';
 import { Fornecedor } from '../models/fornecedor.interface';
 import { FornecedorService } from '../services/fornecedor.service';
 
 @Injectable()
 export class FornecedorReadResolver implements Resolve<Array<Fornecedor>> {
-  constructor(private fornecedorService: FornecedorService) {}
+  constructor(
+    private loadingService: LoadingService,
+    private fornecedorService: FornecedorService
+  ) {}
 
   public resolve(): Observable<Array<Fornecedor>> {
-    return this.fornecedorService.read();
+    this.loadingService.show();
+    return this.fornecedorService
+      .read()
+      .pipe(finalize(() => this.loadingService.hide()));
   }
 }
