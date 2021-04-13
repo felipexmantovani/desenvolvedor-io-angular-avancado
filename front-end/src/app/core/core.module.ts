fed7b15dcda847e731eb91648214da12fc5d4774
Felipe Mantovani <felipexmantovani@gmail.com>
2021-04-12 21:59:59 -0300

    Altera estrutura

--- front-end/src/app/core/core.module.ts
+++ front-end/src/app/core/core.module.ts
@@ -1,17 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { HttpCustomInterceptorModule } from './modules/http-custom-interceptor/http-custom-interceptor.module';
-import { IbgeModule } from './modules/ibge/ibge.module';
 import { LoadingModule } from './modules/loading/loading.module';
-import { ViaCepModule } from './modules/via-cep/via-cep.module';
 
 @NgModule({
   imports: [
     HttpClientModule,
     HttpCustomInterceptorModule,
     LoadingModule,
-    ViaCepModule,
-    IbgeModule
   ],
   exports: [LoadingModule]
 })
