44f59a2d526e3de8cac881d8130b387d6f48d529
Felipe Mantovani <felipexmantovani@gmail.com>
2021-04-27 23:59:25 -0300

    Configura currency R$

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LayoutComponentsModule } from './modules/layout/components/layout-component.module';
+registerLocaleData(pt);
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,5 +17,15 @@ import { LayoutComponentsModule } from './modules/layout/components/layout-compo
     CoreModule
   ],
   bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'R$'
+    }
+  ]
 })
 export class AppModule {}
