042d0d35e799501d399436b4cd1ca8f06fc17d68
Felipe Mantovani <felipexmantovani@gmail.com>
2021-05-06 23:28:33 -0300

    Update fornecedor-combo.directive.spec.ts

--- front-end/src/app/modules/fornecedor/directives/fornecedor-combo/fornecedor-combo.directive.spec.ts
+++ front-end/src/app/modules/fornecedor/directives/fornecedor-combo/fornecedor-combo.directive.spec.ts
@@ -1,3 +1,45 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgControl } from '@angular/forms';
+import { PoComboComponent } from '@po-ui/ng-components';
+import { FornecedorService } from '../../services/fornecedor.service';
+import { FornecedorComboDirective } from './fornecedor-combo.directive';
+
 describe('fornecedor-combo.directive.spec | FornecedorComboDirective', () => {
-  it('Deve ser criado', () => {});
+  let directive: FornecedorComboDirective;
+  let poComboComponent: PoComboComponent;
+  let fixture: ComponentFixture<PoComboComponent>;
+
+  const fornecedorService = jasmine.createSpyObj<FornecedorService>(['getFilteredData', 'getObjectByValue']);
+
+  const ngControl = jasmine.createSpyObj<NgControl>(['control']);
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        FornecedorComboDirective
+      ],
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: FornecedorService,
+          useValue: fornecedorService
+        }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    directive = new FornecedorComboDirective(null, null, null);
+    fixture = TestBed.createComponent(PoComboComponent);
+    poComboComponent = fixture.componentInstance;
+    directive['host'] = [poComboComponent];
+    directive['ngControl'] = ngControl;
+  });
+
+  it('Deve ser criado', () => {
+    directive.ngOnInit();
+    directive.combo.filterService = fornecedorService;
+    expect(directive).toBeTruthy();
+  });
 });
