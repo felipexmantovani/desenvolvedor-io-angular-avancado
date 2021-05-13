5b4e39b24343337e8f7b650f5397135fa0e81f1d
Felipe Mantovani <felipexmantovani@gmail.com>
2021-05-12 21:23:49 -0300

    Update fornecedor-get-by-id.resolver.spec.ts

--- front-end/src/app/modules/fornecedor/resolvers/fornecedor-get-by-id.resolver.spec.ts
+++ front-end/src/app/modules/fornecedor/resolvers/fornecedor-get-by-id.resolver.spec.ts
@@ -1,20 +1,51 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpRequest } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { APP_CONFIG } from '../../../app.config';
+import { FORNECEDOR_MOCK } from '../../../mocks/fornecedor.mock';
+import { FORNECEDOR_CONFIG } from '../fornecedor.config';
 import { FornecedorGetByIdResolver } from './fornecedor-get-by-id.resolver';
 
 describe('fornecedor-get-by-id.resolver.spec | FornecedorGetByIdResolver', () => {
-  let guard: FornecedorGetByIdResolver;
+
+  let resolver: FornecedorGetByIdResolver;
+  let httpTestingController: HttpTestingController;
+
+  const activatedRouteSnapshot = {
+    params: {
+      id: '123'
+    }
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
     });
 
-    guard = TestBed.inject(FornecedorGetByIdResolver);
+    resolver = TestBed.inject(FornecedorGetByIdResolver);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('Deve ser criado', () => {
-    expect(guard).toBeTruthy();
+    expect(resolver).toBeTruthy();
   });
+
+  it('Deve executar endpoint readById no método resolve() e retornar o fornecedor', () => {
+    resolver.resolve(activatedRouteSnapshot as any).subscribe((res) => {
+      expect(res).toEqual(FORNECEDOR_MOCK[0]);
+    });
+
+    httpTestingController.expectOne((req: HttpRequest<any>) => {
+      return (
+        req.url === `${APP_CONFIG.apiV1}${FORNECEDOR_CONFIG.pathApi}/123` &&
+        req.method === 'GET'
+      );
+    })
+    .flush(FORNECEDOR_MOCK[0]);
+  });
+
 });
