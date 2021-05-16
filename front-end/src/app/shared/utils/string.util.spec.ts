e7159e32cf0e6b046fda2c27858bc88ca173f2e5
Felipe Mantovani <felipexmantovani@gmail.com>
2021-05-16 12:01:01 -0300

    Cobertura de teste unitário de isValidCpf e isValidCnpj

--- front-end/src/app/shared/utils/string.util.spec.ts
+++ front-end/src/app/shared/utils/string.util.spec.ts
@@ -1,11 +1,19 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { StringUtil } from './string.util';
 
-describe('string.util.spec | StringUtil', () => {
+fdescribe('string.util.spec | StringUtil', () => {
 
   let textResume: string;
 
+  let form: FormGroup;
+
   beforeEach(() => {
     textResume = 'Programador';
+
+    form = new FormBuilder().group({
+      cpf: [null, [StringUtil.isValidCpf()]],
+      cnpj: [null, [StringUtil.isValidCnpj()]]
+    });
   });
 
   describe('resume()', () => {
@@ -59,6 +67,65 @@ describe('string.util.spec | StringUtil', () => {
     it('Deve aplicar máscara de CNPJ corretamente', () => {
       const cpf = '75130307000106';
       expect(StringUtil.maskCnpj(cpf)).toBe('75.130.307/0001-06');
+    });
+  });
+
+  describe('isValidCpf()', () => {
+    it('Não deve conter erro { cpfInvalido: true } caso valor for null', () => {
+      form.get('cpf').setValue(null);
+      expect(form.get('cpf').errors).not.toEqual({ cpfInvalido: true });
+    });
+
+    it('Deve conter erro { cpfInvalido: true } caso não tiver número de caracteres necessários', () => {
+      form.get('cpf').setValue('123');
+      expect(form.get('cpf').errors).toEqual({ cpfInvalido: true });
+    });
+
+    it('Deve conter erro { cpfInvalido: true } caso for um CPF inválido', () => {
+      form.get('cpf').setValue('00985777465');
+      expect(form.get('cpf').errors).toEqual({ cpfInvalido: true });
+    });
+
+    it('Deve conter erro { cpfInvalido: true } caso todos os números forem iguais', () => {
+      form.get('cpf').setValue('99999999999');
+      expect(form.get('cpf').errors).toEqual({ cpfInvalido: true });
+    });
+
+    it('Não deve conter erro { cpfInvalido: true } caso CPF seja válido', () => {
+      form.get('cpf').setValue('65095130050');
+      expect(form.get('cpf').errors).not.toEqual({ cpfInvalido: true });
+    });
+
+    it('Deve conter erro { cpfInvalido: true } caso dígito verificador for incorreto', () => {
+      form.get('cpf').setValue('65095130051');
+      expect(form.get('cpf').errors).toEqual({ cpfInvalido: true });
+    });
+  });
+
+  describe('isValidCnpj()', () => {
+    it('Não deve conter erro { cnpjInvalido: true } caso valor for null', () => {
+      form.get('cnpj').setValue(null);
+      expect(form.get('cnpj').errors).not.toEqual({ cnpjInvalido: true });
+    });
+
+    it('Deve conter erro { cnpjInvalido: true } caso todos os números forem iguais', () => {
+      form.get('cnpj').setValue('99999999999999');
+      expect(form.get('cnpj').errors).toEqual({ cnpjInvalido: true });
+    });
+
+    it('Deve conter erro { cnpjInvalido: true } caso não tiver número de caracteres necessários', () => {
+      form.get('cnpj').setValue('123');
+      expect(form.get('cnpj').errors).toEqual({ cnpjInvalido: true });
+    });
+
+    it('Não deve conter erro { cnpjInvalido: true } caso CNPJ seja válido', () => {
+      form.get('cnpj').setValue('48568704000191');
+      expect(form.get('cnpj').errors).not.toEqual({ cnpjInvalido: true });
+    });
+
+    it('Deve conter erro { cnpjInvalido: true } caso dígito verificador for incorreto', () => {
+      form.get('cnpj').setValue('48568704000190');
+      expect(form.get('cnpj').errors).toEqual({ cnpjInvalido: true });
     });
   });
 
